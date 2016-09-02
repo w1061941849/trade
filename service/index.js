0078e7c77547b747bd2a08ab0381f3f819288d1c
bob143 <18701806580@163.com>
2016-09-02 17:27:06 +0800

    111

--- service/index.js
+++ service/index.js
@@ -1,34 +1,46 @@
 var httpUtil=require('../utils/http.js')
 var async= require('async');
 exports.showHtml = function (req, res, next) {   
-     // "path":"1/recommenditemlist"  "path":"categorylist"
-    var objs = [{path:'categorylist',"name":"categorylist"}, {path:'1/recommenditemlist',"name":"list1"}, {path:'2/recommenditemlist',"name":"list2"}];  
-  	var results={}; 
-  	function dosomething(obj,done){
-  		console.log(obj) 
-  		var options={
-	         "path":obj.path
-	   	}  
-	   	httpUtil.get(options,function(result,err){ 
-	   		results[obj.name]=result;
-	   		console.log(results)
-	        if(err){
-	        	done(err)
-	        } 
-	   	})  
-  	}
-	async.each(objs, function(obj, callback) {  
-		dosomething(obj,function(err){  
-		 	if(err){
-		 		callback(err);  
-		 	}
-		})
-	    
-	}, function(err){  
-	    if(err){ 
-	    	console.log("err is:" + err);  
-	    }
-	});  
-	res.render('index',{"results":results})
+    	 
+    	  
+    async.series({
+        categorylist: function(callback){
+            var options={ "path":"categorylist" }  
+	  		httpUtil.get(options,function(result,err){ 
+		   		if(err){
+		   			callback(err, null);
+		   		}else{  
+		   			callback(null, result);
+		   		} 
+		   	}) 
+        },
+        list1: function(callback){
+            var options={ "path":"1/recommenditemlist" }  
+	  		httpUtil.get(options,function(result,err){ 
+		   		if(err){
+		   			callback(err, null);
+		   		}else{  
+		   			callback(null, result);
+		   		} 
+		   	}) 
+        } , 
+        list2: function(callback){
+            var options={ "path":"2/recommenditemlist" }  
+	  		httpUtil.get(options,function(result,err){ 
+		   		if(err){
+		   			callback(err, null);
+		   		}else{  
+		   			callback(null, result);
+		   		} 
+		   	}) 
+        } 
 
+    },
+    function(err, results) {   
+    	//console.log(results) 
+    	res.render('index',{"results":results}) 	
+    });
+   
+ //	res.render('index',{'results':results});
+	
 }; 
